27085df3b37af235b57d426d10326ee76d6836d5
A. Can Gencdogus <a.cangencdogus@As-MacBook-Air.local>
2024-08-21 21:04:38 -0400

    formatted my code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "my_vpc" {
   # cidr_block = $vpc_cidr_block
   # cidr_block = ${{ secrets.vpc_cidr_block }} sensitive
   # cidr_block = ${{ var.vpc_cidr_block }} nonsensitive
-  tags       = var.tag
+  tags = var.tag
 }
 
 variable "vpc_cidr_block" {}
--- my_main.tf
+++ my_main.tf
@@ -27,23 +27,23 @@ module "remote_module" {
   # source = terraform-aws-security-groups-027
   source  = "app.terraform.io/027-spring-cloud/security-groups-027/aws"
   version = "1.0.0"
-  vpc_id = module.my_vpc.my_vpc_id
+  vpc_id  = module.my_vpc.my_vpc_id
 
   security_groups = {
     "web" = {
       "description" = "Security Group for Web Tier"
       "ingress_rules" = [
         {
-          to_port = 0
-          from_port = 0
+          to_port     = 0
+          from_port   = 0
           cidr_blocks = ["0.0.0.0/0"]
-          protocol = "-1"
+          protocol    = "-1"
         },
         {
-          to_port = 2
-          from_port = 2
+          to_port     = 2
+          from_port   = 2
           cidr_blocks = ["0.0.0.0/0"]
-          protocol = "tcp"
+          protocol    = "tcp"
         }
       ]
     },
@@ -51,17 +51,17 @@ module "remote_module" {
       "description" = "xvyz"
       "egress_rule s" = [
         {
-          to_port = 0
-          from_port = 0
-          protocol = "tcp"
+          to_port     = 0
+          from_port   = 0
+          protocol    = "tcp"
           cidr_blocks = ["0.0.0.0/0"]
         }
       ]
       "ingress_rules" = [
         {
-          to_port = 0 # 1
-          from_port = 0 # 1
-          protocol = "tcp" #3
+          to_port     = 0     # 1
+          from_port   = 0     # 1
+          protocol    = "tcp" #3
           cidr_blocks = ["0.0.0.0/0"]
         }
       ]
